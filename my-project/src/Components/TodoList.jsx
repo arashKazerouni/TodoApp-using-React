6b8e5cbd03e11925add63678cbe3b3a584c7c4a0
Arash Kazerouni <arashkazerooni484@gmail.com>
2023-05-16 15:15:39 +0330

    some styles appended

--- my-project/src/Components/TodoList.jsx
+++ my-project/src/Components/TodoList.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const TodoList = ({ list }) => {
   const mappedTodoList = list.map((item) => {
     return (
-      <li className="card flex" key={item.id}>
+      <li
+        className="card flex hover:cursor-pointer hover:brightness-110 transition-all duration-200"
+        key={item.id}
+      >
         {item.todo}
         <div className="buttons ml-auto flex gap-2">
           <button className="btn">Edit</button>
